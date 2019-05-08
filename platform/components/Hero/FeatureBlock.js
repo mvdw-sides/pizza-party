17fd516f1930443868a98c000ea7e004101e0a09
Matti van de Weem <mvdweem@gmail.com>
2019-05-09 00:09:42 +0200

    replace the placeholder image, with pizza one

--- platform/components/Hero/FeatureBlock.js
+++ platform/components/Hero/FeatureBlock.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import css from "./featureBlock.scss";
 export default class FeatureBlock extends Component {
   constructor(props) {
@@ -14,7 +15,10 @@ export default class FeatureBlock extends Component {
       >
         <div
           className={css.image}
-          style={{ backgroundImage: "url(/static/images/input1.jpg)" }}
+          style={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)"
+          }}
         />
         <div className={css.description}>
           <h5>Some product</h5>
